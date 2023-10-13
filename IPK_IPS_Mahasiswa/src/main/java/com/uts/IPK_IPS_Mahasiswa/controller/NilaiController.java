71c8686776ea7aa4827c06cce37a85c1065c0bd0
Alief Zulvan <depanb187@gmail.com>
2023-10-14 01:51:57 +0700

    add nilai api

--- IPK_IPS_Mahasiswa/src/main/java/com/uts/IPK_IPS_Mahasiswa/controller/NilaiController.java
+++ IPK_IPS_Mahasiswa/src/main/java/com/uts/IPK_IPS_Mahasiswa/controller/NilaiController.java
@@ -5,15 +5,22 @@ import com.uts.IPK_IPS_Mahasiswa.entity.Nilai;
 import com.uts.IPK_IPS_Mahasiswa.entity.User;
 import com.uts.IPK_IPS_Mahasiswa.enumeration.Erole;
 import com.uts.IPK_IPS_Mahasiswa.payload.request.NilaiRequest;
+import com.uts.IPK_IPS_Mahasiswa.payload.response.JwtResponse;
 import com.uts.IPK_IPS_Mahasiswa.payload.response.MessageResponse;
+import com.uts.IPK_IPS_Mahasiswa.payload.response.NilaiResponse;
 import com.uts.IPK_IPS_Mahasiswa.repository.MataKuliahRepository;
 import com.uts.IPK_IPS_Mahasiswa.repository.NilaiRepository;
 import com.uts.IPK_IPS_Mahasiswa.repository.UserRepository;
 import com.uts.IPK_IPS_Mahasiswa.service.UserActiveService;
+import jakarta.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,53 +28,73 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class NilaiController {
-    
+
     @Autowired
     MataKuliahRepository mataKuliahRepository;
-    
+
     @Autowired
     UserRepository userRepository;
-    
+
     @Autowired
     NilaiRepository nilaiRepository;
-    
+
     @Autowired
     UserActiveService userActiveService;
     
+
     @PostMapping("/nilai/create")
-    public ResponseEntity<?> createNilai(@RequestBody NilaiRequest request){
+    public ResponseEntity<?> createNilai(@RequestBody NilaiRequest request) {
         User userActiv = userActiveService.getUserActive();
-        
-        System.out.println("matkul dosen :" +userActiv.getListAllMatkul());
+
+        System.out.println("matkul dosen :" + userActiv.getListAllMatkul());
         if (!userActiv.getListAllMatkul().contains(request.getMataKuliah())) {
             return ResponseEntity.ok(new MessageResponse("Dosen tidak mengampu matkul ini"));
         }
-        
-        System.out.println("id : "+ request.getIdMahasiswa());
-        System.out.println("Tugas : "+ request.getNilaiTugas());
-        System.out.println("matkul :"+ request.getMataKuliah());
+
+        System.out.println("id : " + request.getIdMahasiswa());
+        System.out.println("Tugas : " + request.getNilaiTugas());
+        System.out.println("matkul :" + request.getMataKuliah());
         Optional<User> user = userRepository.findById(request.getIdMahasiswa());
         User u = user.get();
-        
-        System.out.println(u.getRoles().toString().equals("["+Erole.Mahasiswa.toString()+"]"));
-        if(!u.getRoles().toString().equals("["+Erole.Mahasiswa.toString()+"]") ) {
+
+        System.out.println(u.getRoles().toString().equals("[" + Erole.Mahasiswa.toString() + "]"));
+        if (!u.getRoles().toString().equals("[" + Erole.Mahasiswa.toString() + "]")) {
             return ResponseEntity.ok(new MessageResponse("Role bukan mahasiswa"));
         }
-        
+
         Optional<MataKuliah> matkul = mataKuliahRepository.findByName(request.getMataKuliah());
         MataKuliah mk = matkul.get();
-        
+
         Nilai nilai = new Nilai();
-        
+
         nilai.setNilai_Praktikum(request.getNilaiPraktikum());
         nilai.setNilai_Tugas(request.getNilaiTugas());
         nilai.setNilai_UAS(request.getNilaiUAS());
         nilai.setNilai_UTS(request.getNilaiUTS());
         nilai.setUser(u);
         nilai.setMataKuliah(mk);
-        
+
         nilaiRepository.save(nilai);
-        
-      return ResponseEntity.ok(new MessageResponse("Berhasil membuat nilai"));
+
+        return ResponseEntity.ok(new MessageResponse("Berhasil membuat nilai"));
+    }
+
+    @GetMapping("/nilai/mahasiswa/{id}")
+    public ResponseEntity<?> read(@PathVariable("id") int userID) {
+
+        List<Nilai> nilai = nilaiRepository.findByUser_Id(userID);
+    
+        List<NilaiResponse> listres = new ArrayList<>();
+        NilaiResponse nilaires = new NilaiResponse();
+        for (Nilai n : nilai) {
+           nilaires.setMataKuliah(n.getMataKuliah().toString());
+           nilaires.setNilaiPraktikum(n.getNilai_Praktikum());
+           nilaires.setNilaiTugas(n.getNilai_Tugas());
+           nilaires.setNilaiUTS(n.getNilai_UTS());
+           nilaires.setNilaiUAS(n.getNilai_UAS());
+           listres.add(nilaires);
+        }
+
+        return ResponseEntity.ok(listres);
     }
 }
---
+++ IPK_IPS_Mahasiswa/src/main/java/com/uts/IPK_IPS_Mahasiswa/payload/response/NilaiResponse.java
@@ -0,0 +1,19 @@
+
+package com.uts.IPK_IPS_Mahasiswa.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NilaiResponse {
+    private String mataKuliah;
+    private float nilaiTugas;
+    private float nilaiPraktikum;
+    private float nilaiUTS;
+    private float nilaiUAS;
+}
